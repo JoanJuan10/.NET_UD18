56915b1fb92cad5b0f77c073e94dd81798e508fe
JoanJuan10 <iijoanjuan10@gmail.com>
2021-01-20 12:56:56 +0100

    Ejercicio 8

--- Ejercicios_UD18/Ejercicios_UD18/EJ8/Ejercicio8.cs
+++ Ejercicios_UD18/Ejercicios_UD18/EJ8/Ejercicio8.cs
@@ -9,39 +9,83 @@ namespace Ejercicios_UD18
     {
         public void Ejercicio()
         {
-            Database tiendaInformatica = new Database("Data Source=192.168.1.53;Initial Catalog=master;Persist Security Info=True;User ID=user123;Password=123");
-            tiendaInformatica.ejecutarSQL("CREATE DATABASE ex1");
+            Database almacenes = new Database("Data Source=192.168.1.53;Initial Catalog=master;Persist Security Info=True;User ID=user123;Password=123");
+            almacenes.ejecutarSQL("CREATE DATABASE ex8");
             try
             {
-                tiendaInformatica.Conexion = new SqlConnection("Data Source=192.168.1.53;Initial Catalog=ex1;Persist Security Info=True;User ID=user123; Password=123");
+                almacenes.Conexion = new SqlConnection("Data Source=192.168.1.53;Initial Catalog=ex8;Persist Security Info=True;User ID=user123; Password=123");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-            tiendaInformatica.ejecutarSQL("CREATE TABLE Fabricantes " +
-                "(Codigo INT NOT NULL IDENTITY(1, 1)," +
-                "Nombre NVARCHAR(100)," +
-                "CONSTRAINT pk_fabricantes PRIMARY KEY(Codigo)); ");
-            tiendaInformatica.ejecutarSQL("CREATE TABLE Articulos (" +
+            almacenes.ejecutarSQL("CREATE TABLE Cajeros (" +
+                "Codigo INT NOT NULL IDENTITY(1, 1)," +
+                "NomApels NVARCHAR(255)," +
+                "CONSTRAINT pk_cajeros PRIMARY KEY(Codigo)); ");
+
+            almacenes.ejecutarSQL("CREATE TABLE Productos (" +
                 "Codigo INT NOT NULL IDENTITY(1, 1)," +
                 "Nombre NVARCHAR(100)," +
                 "Precio INT," +
-                "Fabricante INT," +
-                "CONSTRAINT pk_articulos PRIMARY KEY(Codigo)," +
-                "CONSTRAINT fk_articulos FOREIGN KEY(Fabricante) REFERENCES Fabricantes(Codigo)); ");
-            tiendaInformatica.ejecutarSQL("INSERT INTO Fabricantes (Nombre) VALUES " +
-                "('Fabricante1')," +
-                "('Fabricante2')," +
-                "('Fabricante3')," +
-                "('Fabricante4')," +
-                "('Fabricante5');");
-            tiendaInformatica.ejecutarSQL("INSERT INTO Articulos (Nombre, Precio, Fabricante) VALUES " +
-                "('Articulo1',10,1)," +
-                "('Articulo2',10,2)," +
-                "('Articulo3',10,3)," +
-                "('Articulo4',10,4)," +
-                "('Articulo5',10,5);");
+                "CONSTRAINT pk_productos PRIMARY KEY(Codigo)); ");
+            almacenes.ejecutarSQL("CREATE TABLE maquinas_registradas (" +
+                "Codigo INT NOT NULL IDENTITY(1, 1)," +
+                "Piso INT," +
+                "CONSTRAINT pk_maquinas PRIMARY KEY(Codigo)); ");
+            almacenes.ejecutarSQL("CREATE TABLE Venta (" +
+                "Cajero INT," +
+                "Maquina INT," +
+                "Producto INT," +
+                "CONSTRAINT pk_venta PRIMARY KEY(Cajero, Maquina, Producto)," +
+                "CONSTRAINT fk_venta1 FOREIGN KEY(Cajero) REFERENCES Cajeros(Codigo)," +
+                "CONSTRAINT fk_venta2 FOREIGN KEY(Maquina) REFERENCES maquinas_registradas(Codigo)," +
+                "CONSTRAINT fk_venta3 FOREIGN KEY(Producto) REFERENCES Productos(Codigo)); ");
+            almacenes.ejecutarSQL("INSERT INTO Cajeros (NomApels) VALUES" +
+                "('Cajero1')," +
+                "('Cajero2')," +
+                "('Cajero3')," +
+                "('Cajero4')," +
+                "('Cajero5')," +
+                "('Cajero6')," +
+                "('Cajero7')," +
+                "('Cajero8')," +
+                "('Cajero9')," +
+                "('Cajero10'); ");
+            almacenes.ejecutarSQL("INSERT INTO Productos (Nombre, Precio) VALUES" +
+                "('Producto1', 362)," +
+                "('Producto2', 461)," +
+                "('Producto3', 26)," +
+                "('Producto5', 53)," +
+                "('Producto6', 10)," +
+                "('Producto7', 90)," +
+                "('Producto8', 112)," +
+                "('Producto9', 164)," +
+                "('Producto10', 265); ");
+            almacenes.ejecutarSQL("INSERT INTO maquinas_registradas (Piso) VALUES" +
+                "(1)," +
+                "(2)," +
+                "(3)," +
+                "(4)," +
+                "(5)," +
+                "(6)," +
+                "(7)," +
+                "(8)," +
+                "(9)," +
+                "(10); ");
+
+            almacenes.ejecutarSQL("INSERT INTO Venta (Cajero,Maquina,Producto) VALUES" +
+                "(1, 6, 3)," +
+                "(2, 1, 8)," +
+                "(3, 8, 5)," +
+                "(4, 9, 7)," +
+                "(5, 4, 8)," +
+                "(6, 2, 3)," +
+                "(7, 7, 2)," +
+                "(8, 1, 5)," +
+                "(9, 6, 4)," +
+                "(10, 8, 6); ");
+            almacenes.cerrarConexion();
 
         }
     }
